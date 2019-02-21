18fc69dc5f2201b9b68703567d371c7a0754fb0d
Rowan Taylor <rowan.taylor@colorado.edu>
2019-02-21 00:47:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function happyHolidayTo(holiday, name) {
 };
 
 function holidayCountdown(days, holiday) {
-  return `It's ${days} days until ${holiday}!`;
+  return `It\'s ${days} days until ${holiday}!`;
 };
